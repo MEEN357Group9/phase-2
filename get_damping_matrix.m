9328135c79b902cbc11fd9111220802e0fa4497c
soniamelsanchez <32579896+soniamelsanchez@users.noreply.github.com>
2017-10-22 12:41:00 -0500

    mass matrix

--- get_damping_matrix.m
+++ get_damping_matrix.m
@@ -11,14 +11,23 @@ function C = get_damping_matrix( vibration_model, FSAE_Race_Car )
 %                                        damping in the appropriate places
 
 
+% valid inputs
+ad1 = 'quarter_car_1_DOF';
+ad2 = 'quarter_car_2_DOF';
+ad3 = 'half_car_2_DOF';
+ad4 = 'half_car_4_DOF';
+
+
 % input error checking
 if ~ischar(vibration_model) 
+    % the vibration model input is not a string
     error('vibration_model must be a string');
-elseif strcmp('quarter_car_1_DOF',vibration_model) || strcmp('quarter_car_2_DOF',...
-        vibration_model) 
-    
-elseif ~strcmp('quarter_car_1_DOF',vibration_model) && ~strcmp('quarter_car_2_DOF',...
-        vibration_model) 
+elseif strcmp(ad1,vibration_model) || strcmp(ad2,vibration_model) ...
+        || strcmp(ad3,vibration_model) || strcmp(ad4, vibration_model)
+    % all clear for run
+elseif ~strcmp(ad1,vibration_model) && ~strcmp(ad2,vibration_model) ...
+        && ~strcmp(ad3,vibration_model) && ~strcmp(ad4,vibration_model)
+    % not the proper string
     error('The string for vibration_model must be either ''quarter_car_1_DOF'' or  ''quarter_car_2_DOF''.');
     
 elseif ~isstruct(FSAE_Race_Car) 
@@ -27,7 +36,7 @@ else
 end
 
 if strcmp('quarter_car_1_DOF', vibration_model)
-    % For 1 DOF
+    % For quarter car 1 DOF
     % For front suspension
     LRF = get_leverage_ratio('front', FSAE_Race_Car);
     CF = LRF * FSAE_Race_Car.suspension_front.c;
@@ -42,7 +51,7 @@ if strcmp('quarter_car_1_DOF', vibration_model)
     
     
 elseif strcmp('quarter_car_2_DOF', vibration_model)
-    % For 2 DOF
+    % For quarter car 2 DOF
     % For front suspension
     LRF = get_leverage_ratio('front', FSAE_Race_Car);
     CF = LRF * FSAE_Race_Car.suspension_front.c;
@@ -58,6 +67,36 @@ elseif strcmp('quarter_car_2_DOF', vibration_model)
     CW = ( FSAE_Race_Car.wheel_front.c + FSAE_Race_Car.wheel_rear.c )/2*12; % gives units of lb/(ft/sec)
     
     C = [ avgC -avgC; -avgC avgC+CW];
+    
+elseif strcmp(ad3, vibration_model)
+    % For half car 2 DOF
+    
+    % note: l is full length of chassis; lf is the length from the front of the FSAE racecar to the
+    % center of gravity; lr is the length from the rear of the FSAE racecar
+    % to the center of gravity
+    
+    % getting the lengths 
+    l = FSAE_Race_Car.chassis.length;
+    cg = get_cg(FSAE_Race_Car); % ft
+    lf = cg; % ft
+    lr = l - cg; % ft
+    
+elseif strcmp(ad4, vibration_model)
+    % For half car 4 DOF
+    
+    % note: l is full length of chassis; lf is the length from the front of the FSAE racecar to the
+    % center of gravity; lr is the length from the rear of the FSAE racecar
+    % to the center of gravity
+    
+    % getting the lengths 
+    l = FSAE_Race_Car.chassis.length;
+    cg = get_cg(FSAE_Race_Car); % ft
+    lf = cg; % ft
+    lr = l - cg; % ft
+    
+    
+else
+    error('something went wrong')
 end
 
 end
--- get_mass_matrix.m
+++ get_mass_matrix.m
@@ -13,16 +13,20 @@ function M = get_mass_matrix( vibration_model, FSAE_Race_Car )
 % valid inputs
 ad1 = 'quarter_car_1_DOF';
 ad2 = 'quarter_car_2_DOF';
+ad3 = 'half_car_2_DOF';
+ad4 = 'half_car_4_DOF';
 
 
 % input error checking
 if ~ischar(vibration_model) 
+    % the vibration model input is not a string
     error('vibration_model must be a string');
-elseif strcmp('quarter_car_1_DOF',vibration_model) || strcmp('quarter_car_2_DOF',...
-        vibration_model) 
-    
-elseif ~strcmp('quarter_car_1_DOF',vibration_model) && ~strcmp('quarter_car_2_DOF',...
-        vibration_model) 
+elseif strcmp(ad1,vibration_model) || strcmp(ad2,vibration_model) ...
+        || strcmp(ad3,vibration_model) || strcmp(ad4, vibration_model)
+    % all clear for run
+elseif ~strcmp(ad1,vibration_model) && ~strcmp(ad2,vibration_model) ...
+        && ~strcmp(ad3,vibration_model) && ~strcmp(ad4,vibration_model)
+    % not the proper string
     error('The string for vibration_model must be either ''quarter_car_1_DOF'' or  ''quarter_car_2_DOF''.');
     
 elseif ~isstruct(FSAE_Race_Car) 
@@ -32,25 +36,66 @@ end
 
 % Determining and producing the matrix
 if strcmp(ad1,vibration_model) == 1
-    % M matrix for 1 DOF
+    % M matrix for quarter car 1 DOF
+    % excludes the mass of the wheel assembly
     % chassis weight plus driver weight plus motor weight divided by 4 for
     % quarter car and then divided by 32.17 for slugs 
+    
     m = ( FSAE_Race_Car.chassis.weight + FSAE_Race_Car.pilot.weight ...
         + FSAE_Race_Car.power_plant.weight ) / 4 / 32.17 ; % slugs
     
-    M = [m 0; 0 0];
+    % matrix form 
+    M = [m];
     
 elseif strcmp(ad2,vibration_model) == 1
-    % M matrix for 2 DOF
+    % M matrix for quarter car 2 DOF
+    % includes the mass of the wheel assembly
     % chassis weight plus driver weight plus motor weight divided by 4 for
     % quarter car and then divided by 32.17 for slugs 
+    
     m = ( FSAE_Race_Car.chassis.weight + FSAE_Race_Car.pilot.weight ...
         + FSAE_Race_Car.power_plant.weight )/4 /32.17 ; % slugs
     % average of the weight of the tires
     mw = ( FSAE_Race_Car.wheel_front.weight + FSAE_Race_Car.wheel_rear.weight )...
         / 2 / 32.17; % slugs
     
+    % matrix form
     M = [m 0; 0 mw];
+    
+elseif strcmp(ad3, vibration_model) == 1
+    % M matrix for half car 2 DOF
+    % excludes the mass of the wheel assembly
+    % chassis weight plus driver weight plus motor weight divided by 2 for
+    % half car and then divided by 32.17 for slugs 
+    m = ( FSAE_Race_Car.chassis.weight + FSAE_Race_Car.pilot.weight ...
+        + FSAE_Race_Car.power_plant.weight ) / 2 / 32.17 ; % slugs
+    
+    % the moment of interia
+    J = get_Jy(FSAE_Race_Car); % slug ft^2
+    
+    % matrix form
+    M = [m 0; 0 J];
+    
+elseif strcmp(ad4, vibration_model) == 1
+    % M matrix for half car 4 DOF
+    % includes the mass of the wheel assembly but the masses are individual
+    % chassis weight plus driver weight plus motor weight divided by 2 for
+    % half car and then divided by 32.17 for slugs 
+    m = ( FSAE_Race_Car.chassis.weight + FSAE_Race_Car.pilot.weight ...
+        + FSAE_Race_Car.power_plant.weight ) / 2 / 32.17 ; % slugs
+    
+    % the moment of interia 
+    J = get_Jy(FSAE_Race_Car); % slug ft^2
+    
+    % front wheel mass 
+    mf = ( FSAE_Race_Car.wheel_front.weight ) / 32.17; % slugs
+    
+    % rear wheel mass
+    mr = ( FSAE_Race_Car.wheel_rear.weight ) / 32.17; % slugs
+    
+    % mass matrix
+    M = [m 0 0 0; 0 J 0 0; 0 0 mr 0; 0 0 0 mr];
+    
 else
     error('something went wrong');
 end
