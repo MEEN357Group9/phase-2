3fb6e7616b6261627dfeaf55775cff6fcae6b248
soniamelsanchez <32579896+soniamelsanchez@users.noreply.github.com>
2017-11-27 17:03:04 -0600

    yuy

--- ff_2016_2.m
+++ ff_2016_2.m
@@ -26,15 +26,15 @@ value1 = 0;
 value2 = 0;
 value3 = FSAE_Race_Car;
 value4 = 'quarter_car_2_DOF';
-value5 = @trajectory;
+value5 = @(t,X,h,t_in,t_out,V_in,V_out,FSAE_Race_Car)trajectory(t,X,h,t_in,t_out,V_in,V_out,FSAE_Race_Car);
 value6 = 0; % sec
 value7 = 1; % sec
 value8 = 60; %mph
 value9 = 60; % mph
 value10 = 2500; 
-value11 = @tar_strip;
+value11 = @(wheelbase,X_enter,X,V)tar_strip(wheelbase,X_enter,X,V);
 value12 = 1; % ft
-value13 = @tar_strip;
+value13 = @(wheelbase,X_enter,X,V)tar_strip(wheelbase,X_enter,X,V);
 value14 = 1; % ft
 
 % Now to set up the struct for the car
